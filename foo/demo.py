2e3d3583fc7b63958406e2f51dd563c61718093d
Amanibhavam <iam@defn.sh>
2022-05-20 16:52:30 +0000

    use vault, boundary providers

--- foo/demo.py
+++ foo/demo.py
@@ -3,8 +3,10 @@ from cdktf_cdktf_provider_github import GithubProvider
 from cdktf_cdktf_provider_null import NullProvider
 from cdktf_cdktf_provider_tfe import TfeProvider
 from constructs import Construct
+from defn_cdktf_provider_boundary.boundary import BoundaryProvider
 from defn_cdktf_provider_buildkite.buildkite import BuildkiteProvider
 from defn_cdktf_provider_cloudflare.cloudflare import CloudflareProvider
+from defn_cdktf_provider_vault.vault import VaultProvider
 
 
 class DemoStack(TerraformStack):
@@ -18,6 +20,8 @@ class DemoStack(TerraformStack):
         TfeProvider(self, "tfe")
         GithubProvider(self, "github", organization="defn")
         CloudflareProvider(self, "cloudflare")
+        VaultProvider(self, "vault", address="")
+        BoundaryProvider(self, "boundary", addr="")
 
         w = NamedRemoteWorkspace(name="bootstrap")
         RemoteBackend(self, organization="defn", workspaces=w)
