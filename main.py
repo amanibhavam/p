ca1bf7e358ca9f7983069298c1ca98f888e67270
Amanibhavam <iam@defn.sh>
2022-05-21 04:01:49 +0000

    main is a typer script

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ import foo.init
 
 """ init must run before cdktf """
 
+import typer
 from cdktf import App
 from cdktf_cdktf_provider_null import Resource
 
 from foo.demo import DemoStack
 
 
-def main():
+def main(name: str = "demo"):
     app = App()
 
-    stack = DemoStack(app, "demo")
+    stack = DemoStack(app, name)
 
     app.synth()
 
 
 if __name__ == "__main__":
-    main()
+    typer.run(main)
